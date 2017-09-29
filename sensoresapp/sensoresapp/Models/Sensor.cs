6177bff7197807147237c91e79f473d9e357aafa
Noelia Beatriz Machuca <nbmachuca.@gylgroup.com>
2017-09-29 00:50:08 -0300

    Sensores > Adaptaciones gráficas de búsqueda

--- sensoresapp/sensoresapp/Models/Sensor.cs
+++ sensoresapp/sensoresapp/Models/Sensor.cs
@@ -12,10 +12,12 @@ namespace sensoresapp.Models
         public int Id { get; set; }
 
         [Display(Name = "Fecha Desde")]
+        [Required(ErrorMessage = "(*) Fecha desde es requerido")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd hh:mm:ss}", ApplyFormatInEditMode = true)]
         public DateTime FechaDesde { get; set; }
 
         [Display(Name = "Fecha Hasta")]
+        [Required(ErrorMessage = "(*) Fecha hasta es requerido")]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd hh:mm:ss}", ApplyFormatInEditMode = true)]
         public DateTime FechaHasta { get; set; }
     }
